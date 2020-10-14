6573e0f5b2e5ad887acce51c4393ca3e5ee80a34
Mr Vitalik <50264775+VitalikObject@users.noreply.github.com>
2020-10-14 15:09:41 +0300

    Add files via upload

---
+++ src/Packets/Messages/Client/SetContentCreator.py
@@ -0,0 +1,29 @@
+from random import choice
+from string import ascii_uppercase
+import json
+
+from Logic.Player import Players
+from database.DataBase import DataBase
+from Packets.Messages.Server.OutOfSync import OutOfSync
+
+from Utils.Reader import BSMessageReader
+
+
+class SetContentCreator(BSMessageReader):
+    def __init__(self, client, player, initial_bytes):
+        super().__init__(initial_bytes)
+        self.player = player
+        self.client = client
+        
+
+    def decode(self):
+        self.iscommand = False
+        self.string = self.read_string()
+        if self.string.lower() == 'reset':
+            DataBase.replaceValue(self, 'gold', 99999)
+            DataBase.replaceValue(self, 'gems', 99999)
+            self.iscommand = True
+
+    def process(self):
+        if self.iscommand:
+            OutOfSync(self.client, self.player).send()
